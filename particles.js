c61b2ead0c6605577c3e0805ce9c4e073f9658f0
anirdh914 <anirutsihrathod@gmail.com>
2026-01-12 21:52:05 +0530

    Add particles.js for particle effects

---
+++ particles.js
@@ -0,0 +1,13 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 70 },
+    color: { value: "#00ffee" },
+    size: { value: 3 },
+    line_linked: {
+      enable: true,
+      color: "#00ffee",
+      opacity: 0.4
+    },
+    move: { speed: 2 }
+  }
+});
